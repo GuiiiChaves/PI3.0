1bd84c4a01345aa1837835223e4a2f4bf12d9cfb
Guilherme Chaves Sena <114080615+GuiiiChaves@users.noreply.github.com>
2023-10-06 20:59:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
-
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('cache-v1').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/css/style.css',
-          '/js/main.js',
-          '/logosuaptour.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
+  event.waitUntil(
+    caches.open('cache-v1').then(function(cache) {
+      return cache.addAll([
+        '/', // Certifique-se de que sua página inicial está aqui se for "/index.html".
+        '/index.html',
+        '/css/style.css',
+        '/js/main.js',
+        '/logosuaptour.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
   
